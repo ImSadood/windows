17806ff1050f5324481191d855e8657f1a1a00d2
alvinlollo <143326475+alvinlollo@users.noreply.github.com>
2024-11-11 10:43:39 +0000

    fixed script error

--- setup.sh
+++ setup.sh
@@ -4,10 +4,10 @@
 # set -eux pipefail
 
 apt-get update
-apt-get install ca-certificates curl
-install -m 0755 -d /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-chmod a+r /etc/apt/keyrings/docker.asc
+#apt-get install ca-certificates curl
+#install -m 0755 -d /etc/apt/keyrings
+#curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+#chmod a+r /etc/apt/keyrings/docker.asc
 
 # Add the repository to Apt sources:
 #echo \
